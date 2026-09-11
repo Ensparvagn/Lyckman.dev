58a8dcb1fa5c924cf79e897e9f60b9c531165007
Elias Lyckman <lyckman@chalmers.se>
2026-09-11 16:41:39 +0200

    Catjammin

--- demo_svelte/src/routes/+page.svelte
+++ demo_svelte/src/routes/+page.svelte
@@ -2,24 +2,48 @@
 	import spinner from "./spinner.js"
 	let spin = spinner()
 	function oncomplete(angle){
-		console.log(`Finished at angle ${angle%360}`)
+		console.log(`Finished at angle ${angle%360}`) 
 	}
 </script>
 
-<button on:click={()=>spin.start()}>Start</button>
-<button on:click={()=>spin.stop(oncomplete)}>Stop</button>
-<div style="transform: rotateX({$spin.x}rad) rotateY({$spin.y}rad) rotateZ({$spin.z}rad);"></div>
+<button onclick={()=>spin.start()} style="margin-top:px">Start</button>
+<button onclick={()=>spin.stop(oncomplete)}>Stop</button>
+<div class="face" style="transform: rotateX({$spin.x}rad) rotateY({$spin.y}rad) rotateZ({$spin.z}rad);"></div>
 
-<style>
+
+
+<style> 
 	div {
 		display:inline-block;
 		height:500px;
 		width:500px;
 		background-color:red;
-		display:flex;
 		align-items:center;
 		justify-content:center;
-        background: linear-gradient(rgba(255,0,255),rgb(120, 197, 255));
-        border: 1px solid #35d6b0;
+		border: 1px solid #35d6b0;
+		transform-style: preserve-3d;
 	}
+
+	.face {
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  border: 2px solid white;
+  line-height: 200px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+  background-image: url(https://static2.klipy.com/ii/f87f46a2c5aeaeed4c68910815f73eaf/0c/9d/MkZWWzPE.gif);
+  border: 1px solid #35d6b0;
+  border-radius: 20%;
+  background-size: cover;
+
+}
+.front { transform: rotateY( 0deg) translateZ(100px); }
+.back { transform: rotateY(180deg) translateZ(100px); }
+.left { transform: rotateY(-90deg) translateZ(100px); }
+.right { transform: rotateY( 90deg) translateZ(100px); }
+.top { transform: rotateX( 90deg) translateZ(100px); }
+.bottom { transform: rotateX(-90deg) translateZ(100px); }
 </style>
--- demo_svelte/src/routes/spinner.js
+++ demo_svelte/src/routes/spinner.js
@@ -3,14 +3,25 @@ import {tweened} from "svelte/motion"
 import {cubicInOut } from "svelte/easing"
 import {get} from "svelte/store";
 
+
 export default function spinner()
 {
+	let hej = Math.random()
 	let velocity = tweened(0);//, {easing:cubicInOut , duration:3000}
 	let rotation = writable( {
-        x: 0,
-        y: 0,
-        z: 0
+        x: hej, 
+        y: hej,
+        z: hej
     })
+	let position = writable( {
+		x: 0,
+		y: 0 
+	})
+
+	
+
+	//<!--x' =  xcos(a)-ysin(a), y' = xsin(a) + xcos(a)-->
+	
 
 	let running = false;
 	let prev = undefined;
@@ -18,7 +29,9 @@ export default function spinner()
 		if(!prev)prev = time;
 		let duration = time - prev;
 		prev = time;
-        rotation.update(r => ({ x: r.x + 0.1* get(velocity) * duration,  y: r.y + 0.01 * get(velocity) * duration,  z: r.z + get(velocity) * duration}));
+		
+        rotation.update(r => ({ x: (r.x * Math.cos(0.01) - r.y * Math.sin(0.01)),  y: r.y + 0.01 * get(velocity) * duration,  z: r.z + get(velocity) * duration}));
+		position.update(p => ({x: p.x + 10, y: p.y + 1}));
 		requestAnimationFrame(animate);
 	}
 	
