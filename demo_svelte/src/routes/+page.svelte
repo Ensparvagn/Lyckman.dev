e7d7411dcd926bd8e7b946c0d0093efe94913e0f
Spårvagn <eliaslyckman@me.com>
2026-09-12 17:50:16 +0200

    Header + descripton added

--- demo_svelte/src/routes/+page.svelte
+++ demo_svelte/src/routes/+page.svelte
@@ -8,22 +8,45 @@
 
 
 <div class="banner">
-	<h1 style="text-align: center; color:aliceblue; margin-top: 300px">bajs</h1>
+	<h1 style="text-align: center; color:aliceblue; margin-top: 100px;" >Lorem ipsum dolor | sit amet </h1>
+	<div class="description" style="text-align: center;"> 
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id congue magna. Praesent ac maximus justo. 
+		Suspendisse porttitor condimentum ante, at euismod neque hendrerit non. Aliquam semper lobortis elit, vel ultricies massa posuere in. 
+	</div>
 	
-	<button onclick={()=>spin.start()} style="margin-top:px">Start</button>
+	<!--<button onclick={()=>spin.start()} style="margin-top:px">Start</button>
 	<button onclick={()=>spin.stop(oncomplete)}>Stop</button>
-	<div class="face" style="transform: rotateX({$spin.x}rad) rotateY({$spin.y}rad) rotateZ({$spin.z}rad);"></div>
-	<div class="grid-container" style="padding-top: 400px;"> <!--padding creates space inside the parent-->
-		<div class="item" style="grid-area: box-1"></div>
-		<div class="item" style="grid-area: box-2"></div>
-		<div class="item" style="grid-area: box-3"></div>
-		<div class="item" style="grid-area: box-4"></div>
+	<div class="face" style="transform: rotateX({$spin.x}rad) rotateY({$spin.y}rad) rotateZ({$spin.z}rad);"></div>-->
+	<div class="grid-container" style="padding-top: 200px;"> <!--padding creates space inside the parent-->
+		<div class="item-1" style="grid-area: box-1"></div>
+		<div class="item-3" style="grid-area: box-2"></div>
+		<div class="empty-item" style="grid-area: box-3"></div>
+		<div class="empty-item" style="grid-area: box-4"></div>
 	</div>
 
 </div>
 
 
 <style> 
+	h1 {
+	  font-family: 'Manrope', sans-serif;
+	  font-weight: 800;
+	  font-size: 3rem;
+	  letter-spacing: -0.04em;
+	}
+
+	@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
+
+	.description {
+  		font-family: 'Manrope', sans-serif;
+  		font-size: 1.5rem;
+  		line-height: 1.7;
+  		letter-spacing: -0.01em;
+  		color: #6b7280;
+		max-width: 700px;
+		text-align: center;
+		margin: 0 auto;
+	}
 
 
 	.banner {
@@ -40,19 +63,30 @@
 		background-size: cover
 	}
 
-	
-
-	.item {
+	.item-1 {
+		background-image: url(https://static2.klipy.com/ii/f87f46a2c5aeaeed4c68910815f73eaf/0c/9d/MkZWWzPE.gif);
+		background-size: cover;
+		border-radius: 10px;
+		
+	}
+	.empty-item {
 		color: red;
 		background-color: black;
+		border-radius: 10px;
+		
+	} 
+	.item-3 {
+		background-image: url(https://static2.klipy.com/ii/935d7ab9d8c6202580a668421940ec81/ac/46/FVTkvNhH.gif);
+		background-size: cover;
 		border-radius: 10px;
 		
 	}
 
 	.grid-container {
-		
+		margin-right: 200px;
+		margin-left: 200px;
 		display: grid;
-		grid-template-columns: 400px 400px 400px 300px 300px;
+		grid-template-columns: 4fr 4fr 4fr 3fr 3fr;
 		grid-template-rows: 600px 600px;
 		gap: 1em;
 		justify-content: center;
